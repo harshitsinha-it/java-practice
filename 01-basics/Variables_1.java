8cbd6f09409b933711a017761d50cf4bb1ef59b6
Harshit Kumar Sinha <neetusinha000@gmail.com>
2026-04-22 18:19:33 +0530

    variable-1

---
+++ 01-basics/Variables_1.java
@@ -0,0 +1,14 @@
+public class Variables_1 {
+    public static void main(String[] args){
+        int age = 20 ;
+        double price = 99.99 ;
+        char grade = 'A' ;
+        boolean ispass = true ;
+        String name = "Harshit" ;
+        System.out.println("age is:" +age);
+        System.out.println("price =" +price);
+        System.out.println("Grade is:" +grade);
+        System.out.println("Result is:" +ispass);
+        System.out.println("My name is" +name);
+    }
+}
