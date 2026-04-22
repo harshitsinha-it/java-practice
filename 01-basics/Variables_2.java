5e39c9badb62ed00d7abd0268c87a870d79b7570
Harshit Kumar Sinha <neetusinha000@gmail.com>
2026-04-22 18:31:11 +0530

    Day-2 variables basics

---
+++ 01-basics/Variables_2.java
@@ -0,0 +1,7 @@
+public class Variables_2 {
+    public static void main (String[] args){
+        String name = "Harshit";
+        int age = 19;
+        System.out.println("My name is " + name + "\nAnd I am " + age + " years old");
+    }
+}
