5c3cc5f1850b20fab53e98671d7056626fcbdc7e
Harshit Kumar Sinha <neetusinha000@gmail.com>
2026-04-26 09:19:33 +0530

    little edit

--- 07-patterns/Pattern13.java
+++ 07-patterns/Pattern13.java
@@ -2,19 +2,35 @@ public class Pattern13 {
     public static void main(String[] args){
         System.out.println("Star merge triangle: ");
         for (int i = 1; i <= 4; i++){
+            //space
             for (int j = 1; j <= 4-i; j++){
-                //space
                 System.out.print(" ");
             }
+            //half tringle
             for (int j = 1; j <= i; j++){
                 System.out.print("*");
             }
-            if (i != 1){
-                for (int j = 2; j <= i; j++){
+            //another half triangle
+                for (int j = 1; j < i; j++){
                     System.out.print("*");
-                }
             }
             System.out.println();
         }
+        //for lower part
+        for (int i = 1; i <= 4; i++){
+            for (int j = 1; j < i; j++){
+                System.out.print(" ");
+            }
+            //lower half tringle
+            for (int j = i; j <= 4; j++){
+                System.out.print("*");
+            }
+            //for another lower half triangle
+            for (int j = 4-i; j > 0; j--){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+
     }
 }
