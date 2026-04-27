d1b0ef0db42275ae1127af9907e5758b896f4b38
Harshit Kumar Sinha <neetusinha000@gmail.com>
2026-04-27 21:58:12 +0530

    loop pattern improvement

--- 03-loops/Pattern5.java
+++ 03-loops/Pattern5.java
@@ -1,6 +1,7 @@
 public class Pattern5 {
     public static void main(String[] args){
-        for (int i = 5; i >= 1; i--){
+        int n = 5;
+        for (int i = n; i >= 1; i--){
             for (int j =i; j >= 1; j--){
                 System.out.print(" " +j);
             }
